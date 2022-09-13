37b955a39954e35aa8ca00b929897a4f545cc92e
Marcus <marcus1sp2@gmail.com>
2022-09-13 12:21:40 +0200

    🟢 Successful pull request

--- src/main/java/HttpRequests.java
+++ src/main/java/HttpRequests.java
@@ -1,6 +1,41 @@
+import java.io.IOException;
+import java.net.Socket;
 
 public class HttpRequests {
 
 
 
+    public HttpRequests() throws IOException {
+
+        Socket socket = new Socket("httpbin.org", 80);
+        String request =
+                ("GET /html HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: httpbin.org\r\n" +
+                        "\r\n");
+
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char) c);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        String request =
+                ("GET /html HTTP/1.1\r\n" +
+                "Connection: close\r\n" +
+                "Host: httpbin.org\r\n" +
+                "\r\n");
+
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char) c);
+        }
+    }
 }
