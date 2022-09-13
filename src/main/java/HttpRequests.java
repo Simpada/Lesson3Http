25afea7d60e3ed77afca13d0caf7f386f7d44164
Marcus <marcus1sp2@gmail.com>
2022-09-13 12:29:33 +0200

    🔴 should get successful response to get

--- src/main/java/HttpRequests.java
+++ src/main/java/HttpRequests.java
@@ -3,10 +3,31 @@ import java.net.Socket;
 
 public class HttpRequests {
 
+    public HttpRequests(String host, int port, String requestTarget) throws IOException {
+
+        Socket socket = new Socket(host, port);
+        String request =
+                ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "\r\n");
+
+        socket.getOutputStream().write(request.getBytes());
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            //System.out.print((char) c);
+        }
+
+    }
+
+    public int getStatusCode() {
+
+        return 0;
+    }
 
 
-    public HttpRequests() throws IOException {
-
+    public static void main(String[] args) throws IOException {
         Socket socket = new Socket("httpbin.org", 80);
         String request =
                 ("GET /html HTTP/1.1\r\n" +
@@ -20,22 +41,6 @@ public class HttpRequests {
         while ((c = socket.getInputStream().read()) != -1) {
             System.out.print((char) c);
         }
-
     }
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("httpbin.org", 80);
-        String request =
-                ("GET /html HTTP/1.1\r\n" +
-                "Connection: close\r\n" +
-                "Host: httpbin.org\r\n" +
-                "\r\n");
-
-        socket.getOutputStream().write(request.getBytes());
-
-        int c;
-        while ((c = socket.getInputStream().read()) != -1) {
-            System.out.print((char) c);
-        }
-    }
 }
--- src/test/java/HttpRequestsTest.java
+++ src/test/java/HttpRequestsTest.java
@@ -1,6 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpRequestsTest {
 
+    @Test
+    void shouldGetSuccessfulResponseCode() throws IOException {
 
+        HttpRequests client = new HttpRequests("httpbin.org", 80, "/html");
+
+        assertEquals(200, client.getStatusCode());
+
+    }
 
 }
