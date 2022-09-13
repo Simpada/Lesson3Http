85c25b923964367dd337208c54f4652a2fe977dd
Marcus <marcus1sp2@gmail.com>
2022-09-13 12:33:30 +0200

    🔴 should get failure response to get

--- src/test/java/HttpRequestsTest.java
+++ src/test/java/HttpRequestsTest.java
@@ -9,9 +9,15 @@ public class HttpRequestsTest {
     void shouldGetSuccessfulResponseCode() throws IOException {
 
         HttpRequests client = new HttpRequests("httpbin.org", 80, "/html");
-
         assertEquals(200, client.getStatusCode());
 
+        //client = new HttpRequests("httpbin.org", 80, "/random");
+        //assertEquals(404, client.getStatusCode());
+    }
+    @Test
+    void shouldGetFailureResponseCode() throws IOException {
+        HttpRequests client = new HttpRequests("httpbin.org", 80, "/status/403");
+        assertEquals(403, client.getStatusCode());
     }
 
 }
