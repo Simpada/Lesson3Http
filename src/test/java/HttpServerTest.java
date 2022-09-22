4b26758daf77a0993334eb6ef7c7405d3a82a0d7
Marcus <marcus1sp2@gmail.com>
2022-09-22 13:35:31 +0200

    🔵 reformat tests

--- src/test/java/HttpServerTest.java
+++ src/test/java/HttpServerTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
+
+    private Path serverRoot = Path.of("target", "test-files");
 
     @Test
     void shouldGive404() throws IOException {
@@ -15,9 +20,12 @@ public class HttpServerTest {
 
     @Test
     void shouldReadResponseCode200() throws IOException {
+        Files.createDirectories(serverRoot);
+        Path file = serverRoot.resolve("hello.txt");
+        String content = "Hello " + LocalDateTime.now();
+        Files.writeString(file, content);
         HttpServer server = new HttpServer(0);
-        int port = server.getPort();
-        HttpRequests client = new HttpRequests("localhost", port, "/hello");
+        HttpRequests client = new HttpRequests("localhost", server.getPort(), "/" + file.getFileName());
         assertEquals(200, client.getStatusCode());
     }
 
